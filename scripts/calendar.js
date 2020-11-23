ea26579d5da7114ae75ece4428dad93e912641b4
tkhadir <tkhadir.dev@gmail.com>
2020-11-23 18:14:17 +0100

    :rocket: add calendar

---
+++ scripts/calendar.js
@@ -0,0 +1,48 @@
+let dayjs$ = dayjs()
+
+let getDays = (max) => {
+    let days = []
+    for (let i=1; i<=max; i++) {
+        days.push(i)
+    }
+    return days
+}
+
+
+let init = () => {
+    let calendarEle = document.getElementById('calendar')
+    calendarEle.innerHTML = ''
+    let selectedMonth = document.getElementById('form-month').value - 1
+    let selectedYear = document.getElementById('form-year').value
+
+    
+    let days = dayjs$.year(parseInt(selectedYear)).month(parseInt(selectedMonth)).daysInMonth()
+    getDays(days).forEach(d => {
+        calendarEle.innerHTML += '<button onclick="displayResult('+ d +')">' + d + '</button>'
+        if (d % 5 == 0 ) calendarEle.innerHTML += '</br>'
+    })
+}
+
+let displayResult = (d) => {
+    let selectedMonth = document.getElementById('form-month').value - 1
+    let selectedYear = document.getElementById('form-year').value
+    let selectedDayEle = document.getElementById('result')
+
+    let result = dayjs$.year(parseInt(selectedYear)).month(parseInt(selectedMonth)).date(d).format()
+    selectedDayEle.innerHTML = result
+}
+
+let refresh = () => {
+    let calendarEle = document.getElementById('calendar')
+    calendarEle.innerHTML = ''
+    let selectedMonth = document.getElementById('form-month').value - 1
+    let selectedYear = document.getElementById('form-year').value
+    let selectedDayEle = document.getElementById('result')
+
+    let days = dayjs$.year(parseInt(selectedYear)).month(parseInt(selectedMonth)).daysInMonth()
+    getDays(days).forEach(d => {
+        calendarEle.innerHTML += '<button onclick="displayResult('+ d +')">' + d + '</button>'
+        if (d % 10 == 0 ) calendarEle.innerHTML += '</br>'
+    })
+    selectedDayEle.innerHTML = dayjs$.year(parseInt(selectedYear)).month(parseInt(selectedMonth)).startOf('day').format()
+}
